1d4d02b223f532a433c2cebec1312d9f0ad4c7c7
Gene Boggs <gene.boggs@gmail.com>
2026-09-01 21:04:13 -0700

    Version

--- lib/Music/MelodicDevice/Arpeggiation.pm
+++ lib/Music/MelodicDevice/Arpeggiation.pm
@@ -2,7 +2,7 @@ package Music::MelodicDevice::Arpeggiation;
 
 # ABSTRACT: Apply arpeggiation patterns to groups of notes
 
-our $VERSION = '0.03400';
+our $VERSION = '0.0400';
 
 use Moo;
 use strictures 2;
